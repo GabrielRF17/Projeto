b934a1bf4674546c14e9c488c0223464a25790e0
PixelArtMZ <112896911+PixelArtMZ@users.noreply.github.com>
2022-09-21 14:52:55 -0300

    Update Q1.sh
    Mudanças dos textos.

--- Q1.sh
+++ Q1.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-x1=$(zenity --entry --text "digite primeiro valor")
-x2=$(zenity --entry --text "digite segundo valor")
+x1=$(zenity --entry --text "Digite primeiro valor:")
+x2=$(zenity --entry --text "Digite segundo valor:")
 echo "$x1"
 if [ $x1 -gt $x2 ]; then
-    zenity --info --text="maior numero é $x1"
+    zenity --info --text="Maior número é: $x1"
 	
 else
  if [ $x2 -gt $x1 ]; then
-	zenity --info --text="maior numero é $x2"
+	zenity --info --text="Maior número é: $x2"
   
   else
-  	zenity --info --text="os numeros sao iguais"
+  	zenity --info --text="Os números são iguais!"
   fi
  fi
 soma=$(($x1+$x2))
- zenity --info --text="Soma dos numeros igua a $soma"
+ zenity --info --text="A soma dos números é igual a: $soma"
