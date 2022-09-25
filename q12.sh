e84c9985f5b6146334f1314fd406c46bda441eb0
Igor Gabriel <97804247+igorUfca@users.noreply.github.com>
2022-09-25 09:15:33 -0300

    Atualização na entrada

--- q12.sh
+++ q12.sh
@@ -1,6 +1,6 @@
 
 zenity --list \
     --title="Trabalho" \
-    --column="Usuarios cadastrados" \
+    --column="Usuário Cadastrados" \
     --text "" \
     "$(cat /etc/passwd | grep -E 'home|root' | cut -d : -f 1,6 | sed 's/:/ => /')"
