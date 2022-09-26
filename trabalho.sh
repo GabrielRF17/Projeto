512bf516c336179839d1d4180252f18b6c43fc75
Gabriel <grocha722@gmail.com>
2022-09-26 14:33:07 -0300

    mudanças de texto

--- trabalho.sh
+++ trabalho.sh
@@ -11,16 +11,15 @@ x=$(zenity --list \
     "Questão 4" "exibe todos os numeros pares no intervalo de 0 ate x" \
     "Questão 5" "recebe nome de arquivo como parametro e informa se o mesmo existe e imprime seu tamanho em bytes" \
     "Questão 6" "exibe status partição/usuarios logados/data e hora" \
-    "Questão 7" "ate agora nao entendi o q essa questao pede" \
+    "Questão 7" "Reservar laboratorio" \
     "Questão 8" "da o nome da primeira linha do arquivo como titulo" \
     "Questão 9" "recebe lista de palavaras e exibe em ordem alfabetica" \
-    "Questão 10" "mesma coisa da anterior mas so para quando recebe palavra sair" \
+    "Questão 10" "recebe varias palavras e exibe em ordem alfabedica (programa so encerra ao digitar palavra (sair))" \
     "Questão 11" "calculadora" \
     "Questão 12" "usuarios cadastrados no diretorio home" \
-    "Questão 13" "preguiça" \
+    "Questão 13" "usuarios cadastrados em /etc/passwd" \
 
 )
-echo "opa $x opa"
 [ -n "$x" ] && {
 case "$x" in
 "Questão 1")
